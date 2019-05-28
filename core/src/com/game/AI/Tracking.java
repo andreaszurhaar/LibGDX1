4e8ab61d895fece50f7028bfbf6edfbe7f482569
LukasPad <lukas.padolevicius@gmail.com>
2019-05-28 14:30:40 +0200

    improvements to tracking

--- core/src/com/game/AI/Tracking.java
+++ core/src/com/game/AI/Tracking.java
@@ -3,7 +3,9 @@ package com.game.AI;
 import com.badlogic.gdx.math.Vector2;
 import com.game.Board.Agent;
 import com.game.Board.Guard;
+import com.game.Board.Intruder;
 import com.game.Board.Area;
+import com.game.Board.Board;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
@@ -16,19 +18,42 @@ public class Tracking extends AI {
     private float rotation;
     private float speed;
     private float angle;
+    public Vector2 showvect;
+    private float enemyx;
+    private float enemyy;
+    private AI previousAI;
+    private int trackcounter;
 
-    public Tracking(Guard guard, Agent opponent)
+    public Tracking(Guard guard, Agent opponent,AI storeAI)
     {
     	this.guard = guard;
-        Vector2 toEnemy = new Vector2(opponent.xCenter-guard.xCenter,opponent.yCenter-guard.yCenter);
-        angle = toEnemy.angle(guard.viewAngle);
-        
-        
-        
+    	previousAI = storeAI;
+    	enemyx = opponent.xCenter;
+    	enemyy = opponent.yCenter;
+        trackIntruder();
+        trackcounter = 0;
     }
 
     public void trackIntruder()
     {
+    	System.out.print("retracking");
+    	Vector2 toEnemy = new Vector2(enemyx-guard.xCenter,enemyy-guard.yCenter);
+        angle = toEnemy.angle(guard.viewAngle);
+        showvect = toEnemy;
+        System.out.println("angle of "+angle);
+        if(angle >= 0 && angle < 45) {
+        	speed = 1.4f;
+        	rotation = -45;
+        } else if(angle < 0 && angle > -45) {
+        	speed = 1.4f;
+        	rotation = 45;
+        } else if(angle < 0) {
+        	speed = 0.2f;
+        	rotation = 180;
+        } else {
+        	speed = 0.2f;
+        	rotation = -180;
+        }
         
     }
     
@@ -37,11 +62,18 @@ public class Tracking extends AI {
 
 
     public float getRotation(){
-        return -1;
+        return (float) rotation/Board.fps;
     };
 
     public float getSpeed(){
-        return -1;
+    	trackcounter++;
+    	System.out.println("got speed with counter: "+trackcounter);
+    	if(trackcounter > 10) {
+    		guard.setAI(previousAI);
+    		return guard.ai.getSpeed();
+    	}
+    	trackIntruder();
+        return (float) speed/Board.fps;
     };
 
     public void setAgent(Agent agent){
@@ -70,6 +102,11 @@ public class Tracking extends AI {
 
     @Override
     public void seeAgent(Agent agent) {
-    	
+    	System.out.println("saw agent with counter: "+trackcounter);
+    	if(trackcounter > 3) {trackcounter = 3;}
+    	if(agent instanceof Intruder) {
+	    	enemyx = agent.xCenter;
+	    	enemyy = agent.yCenter;
+    	}
     }
 }
