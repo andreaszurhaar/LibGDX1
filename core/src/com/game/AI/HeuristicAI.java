fef3fec54cfbd32dd5ce201436ddaf8768a4645f
Famke Nouwens <famke2604@gmail.com>
2019-06-14 14:30:40 +0200

    working on heuristic ai random movement

--- core/src/com/game/AI/HeuristicAI.java
+++ core/src/com/game/AI/HeuristicAI.java
@@ -1,10 +1,12 @@
 package com.game.AI;
 
+import com.badlogic.gdx.math.Vector2;
 import com.game.Board.Agent;
 import com.game.Board.Area;
 import com.game.Board.Board;
 import com.game.Board.MapDivider;
 import com.game.CopsAndRobbers;
+import com.game.States.MapState;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
@@ -12,25 +14,27 @@ import java.util.ArrayList;
 public class HeuristicAI extends AI {
 
     private Agent agent;
-//    private float areaWidth,areaHeight;
+    private Vector2 point;
+    //    private float areaWidth,areaHeight;
     private final int FACTOR = 20; //number of squares that we want
     public final static int BOARD_WIDTH = 400;
     public final static int BOARD_HEIGHT = 200;
     private ArrayList<Point2D.Float> explorationPoints;
     private String pattern;
+    public Vector2 startingPoint;
+    public static final float X_REDUC = MapState.X_REDUC;
+    public static final float Y_REDUC = MapState.Y_REDUC;
 
-    public HeuristicAI(Agent agent)
-    {
+    public HeuristicAI(Agent agent) {
         this.agent = agent;
         explorationSetUp();
     }
 
-    public HeuristicAI()
-    {
+    public HeuristicAI() {
         explorationSetUp();
+
     }
 /*
-
     public HeuristicAI(Agent agent, float areaWidth, float areaHeight)
     {
         this.agent = agent;
@@ -40,47 +44,74 @@ public class HeuristicAI extends AI {
     }
 */
 
-    public void explorationSetUp()
-    {
+    public void explorationSetUp() {
 
         explorationPoints = new ArrayList<Point2D.Float>();
-        float tempX = BOARD_WIDTH /FACTOR;
-        float tempY = BOARD_HEIGHT/FACTOR;
-        for (int i = 0; i<FACTOR;i++){
-            for (int j = 0; j<FACTOR; j++)
-            {
-                explorationPoints.add(new Point2D.Float(i*tempX+0.5f*tempX, j*tempY+0.5f*tempY));
+        float tempX = BOARD_WIDTH / FACTOR;
+        float tempY = BOARD_HEIGHT / FACTOR;
+        for (int i = 0; i < FACTOR; i++) {
+            for (int j = 0; j < FACTOR; j++) {
+                explorationPoints.add(new Point2D.Float(i * tempX + 0.5f * tempX, j * tempY + 0.5f * tempY));
             }
         }
 
 
     }
 
-    public void exploration(){
-        if (pattern.equals("snake")){
-            snakeMovement();
+    public void exploration() {
+        if (pattern.equals("snake")) {
+            point = snakeMovement();
+        } else if (pattern.equals("random")) {
+            point = randomMovement();
         }
-        else if (pattern.equals("random")){
-            randomMovement();
-        }
+
+        instruction.translate(point, agent, true);
+        rotation = instruction.getRotations();
+        speed = instruction.getSpeeds();
     }
 
-    private void snakeMovement(){
-
+    private Vector2 snakeMovement() {
+        float startingX = agent.getX();
+        float startingY = agent.getY();
+        point = new Vector2(startingX, startingY);
+        if (point.x > 200) {
+            if (point.y > 100) {
+                point = new Vector2(360, 180);
+            } else {
+                point = new Vector2(360, 30);
+            }
+        } else {
+            if (point.y > 100) {
+                point = new Vector2(30, 180);
+            } else {
+                point = new Vector2(30, 30);
+            }
+        }
+        return point;
     }
 
-    private void randomMovement(){
+    private Vector2 randomMovement() {
 
     }
 
     @Override
     public float getRotation() {
-        return 0;
+        if (rotation.empty()) {
+            exploration();
+        } else {
+            //System.out.print("  and rotation: "+rotation.peek());
+            return rotation.pop();
+        }
     }
 
     @Override
     public float getSpeed() {
-        return 0;
+        if (speed.empty()) {
+            exploration();
+        } else {
+            //System.out.println("  getting instruction to move with speed: "+speed.peek());
+            return speed.pop();
+        }
     }
 
     @Override
@@ -113,4 +144,7 @@ public class HeuristicAI extends AI {
 
     }
 
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
 }
