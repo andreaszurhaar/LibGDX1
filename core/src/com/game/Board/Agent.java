d55ac18402023bbe66b8c75298934706c0ac431f
Andreas <andreaszurhaar@gmail.com>
2019-06-17 14:47:26 +0200

    finished AI

--- core/src/com/game/Board/Agent.java
+++ core/src/com/game/Board/Agent.java
@@ -68,7 +68,8 @@ public class Agent extends AssetManager {
 		viewRadius = 45;
 		hearing = false;
 		renderer = new ShapeRenderer();
-		maxSpeed = 1.4f;
+//		maxSpeed = 1.4f;
+		maxSpeed = 15f;
 		speed = 0f;
 	}
 	
