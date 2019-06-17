a24856b03c361c44468ad133cedb9da79d462a46
yvarhulshof <yjfhulshof@gmail.com>
2019-06-17 16:46:11 +0200

    Astar implemented with moveAwayFromSound

--- core/src/com/game/Board/Guard.java
+++ core/src/com/game/Board/Guard.java
@@ -44,8 +44,8 @@ public class Guard extends Agent {
 		this.height = height;
 		//viewAngle.setToRandomDirection();
 		speed = 1;
-		//maxSpeed = 1.4f;
 		maxSpeed = 15f;
+		//maxSpeed = 1.4f;
 		soundRange = 0;
 		viewRange = 6f + width / 2;
 		name = "2";
